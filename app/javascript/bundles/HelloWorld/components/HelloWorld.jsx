f380cb176efa8078e6bb2a7f2e23834976ecaae2
Fábio Albini Braga <fabio.albini@diffagency.com>
2024-01-22 09:30:45 -0500

    introduces polaris

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import style from './HelloWorld.module.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import {AppProvider, Page, LegacyCard, Button} from '@shopify/polaris';
 
 const HelloWorld = (props) => {
   const [name, setName] = useState(props.name);
@@ -15,6 +17,13 @@ const HelloWorld = (props) => {
           <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
       </form>
+      <AppProvider i18n={enTranslations}>
+      <Page title="Example app">
+        <LegacyCard sectioned>
+          <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+        </LegacyCard>
+      </Page>
+    </AppProvider>
     </div>
   );
 };
--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,5 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
+import '@shopify/polaris/build/esm/styles.css';
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
